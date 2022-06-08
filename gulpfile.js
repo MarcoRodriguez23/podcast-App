0fa202361efbdfbc921a8ffa41fb61f4d523e5ce
Marco Rodriguez <cv5233@innovaccion.mx>
2022-06-07 21:09:01 -0500

    iniciando el proyecto de podcastApp boostrap 5

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const {src,dest,watch,series,parallel} = require ('gulp');
+const sass = require ('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+
+function css(done){
+    //identificar donde esta el archivo
+    src('src/scss/app.scss')
+        //compilar SASS
+        .pipe(sass())
+        //guardarlo en una ubicación
+        .pipe(dest('build/css'))
+
+    done();
+}
+
+function dev(done){
+    watch('src/scss/**/*.scss',css);
+
+    done();
+}
+
+function imagenes(done){
+    //identificar donde esta el archivo
+    src('src/img/**/*')
+        //compilar SASS
+        .pipe(imagemin({
+            optimizationLevel:3
+        }))
+        //guardarlo en una ubicación
+        .pipe(dest('build/img'))
+
+    done();
+}
+
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+
+exports.default = parallel(imagenes,css,dev);
